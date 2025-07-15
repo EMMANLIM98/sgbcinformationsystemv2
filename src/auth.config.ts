295c83c75ff2d8f74e21c6349e2ee655e94806f0
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-07-15 22:26:49 +0800

    SignIn users functionality.

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,6 +1,25 @@
-import GitHub from "next-auth/providers/github"
+import Credentials from "next-auth/providers/credentials"
 import type { NextAuthConfig } from "next-auth"
+import { loginSchema } from "./lib/schemas/loginSchema"
+import { getUserByEmail } from "./app/actions/authActions";
+import { compare } from "bcryptjs";
  
 export default {
-  providers: [GitHub],
+  providers: [Credentials({
+    name: 'credentials',
+    async authorize(creds) {
+      const validate = loginSchema.safeParse(creds);
+
+      if (validate.success) {
+        const {email, password} = validate.data;
+        const user = await getUserByEmail(email);
+
+        if (!user || !(await compare(password, user.passwordHash))) return null;
+
+        return user;
+      }
+
+      return null;
+    }
+  })],
 } satisfies NextAuthConfig
