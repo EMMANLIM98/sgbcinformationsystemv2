35ef5d2dc864e6328453084acc25815e0b66cd1e
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-07-15 17:04:31 +0800

    Setting up error in the form (server side).

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -2,32 +2,44 @@
 
 import { prisma } from "@/lib/prisma";
 import { registerSchema, RegisterSchema } from "@/lib/schemas/registerSchema";
+import { ActionResult } from "@/types";
+import { User } from "@prisma/client";
 import bcrypt from "bcryptjs";
+import { ZodIssue } from "zod/v3";
 
-export async function registerUser(data: RegisterSchema) {
-    const validated = registerSchema.safeParse(data);
+export async function registerUser(data: RegisterSchema): Promise<ActionResult<User>> {
+    try {
+        const validated = registerSchema.safeParse(data);
 
-    if (!validated.success) {
-        return { error: validated.error.issues}
-    }
-
-    const {name, email, password} = validated.data;
-
-    const hashedPassword = await bcrypt.hash(password, 14);
-
-    const existingUser = await prisma.user.findUnique({
-        where: { email: email }        
-    })
-
-    if (existingUser) {
-        return {error: 'User already exists with this email.'}
-    }
-
-    return prisma.user.create({
-        data: {
-            name: name,
-            email: email,
-            passwordHash: hashedPassword
+        if (!validated.success) {
+            return { status: 'error', error: validated.error.issues as ZodIssue[] };
         }
-    })
+
+        const {name, email, password} = validated.data;
+
+        const hashedPassword = await bcrypt.hash(password, 14);
+
+        const existingUser = await prisma.user.findUnique({
+            where: { email: email }        
+        })
+
+        if (existingUser) {
+            return {status: 'error', error: 'User already exists with this email.'}
+        }
+
+        const user = await prisma.user.create({
+            data: {
+                name: name,
+                email: email,
+                passwordHash: hashedPassword
+            }
+        })
+
+        return {status: 'success', data: user}
+    } catch (error) {
+        console.log(error);
+        return {status: 'error', error: 'An unexpected error occurred.'}
+    }
+
+    
 }
