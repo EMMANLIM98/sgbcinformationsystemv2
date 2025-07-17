8664920f92eed14554b4abcb7895029ee8efda14
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-07-17 13:35:34 +0800

    Adding sign out functionality.

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,9 +1,11 @@
 'use client';
 import { HeroUIProvider } from "@heroui/system";
+import { ToastContainer } from "react-toastify";
 
 export default function Providers({children}: {children: React.ReactNode}) {
   return (
     <HeroUIProvider>
+      <ToastContainer position="bottom-right" className="z-50" />
         {children}
     </HeroUIProvider>
   )
