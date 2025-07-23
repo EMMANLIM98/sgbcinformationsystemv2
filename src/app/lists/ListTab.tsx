63aede5490ef27e9ac78286656ad9e7408c5515d
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-07-23 19:43:28 +0800

    Adding useTransition hook and loading component.

--- src/app/lists/ListTab.tsx
+++ src/app/lists/ListTab.tsx
@@ -2,9 +2,10 @@
 
 import { Tab, Tabs } from '@heroui/react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import React, { Key } from 'react'
+import React, { Key, useTransition } from 'react'
 import MemberCard from '../members/MemberCard';
 import { Member } from '@prisma/client';
+import Loading from '@/components/Loading';
 
 type Props = {
     members: Member[];
@@ -15,6 +16,7 @@ export default function ListTab({ members, likeIds }: Props) {
     const searchParams = useSearchParams();
     const router = useRouter();
     const pathname = usePathname();
+    const [ isPending, startTransition ] = useTransition();
 
     const tabs = [
         { id: 'source', label: 'Members I have liked' },
@@ -23,9 +25,11 @@ export default function ListTab({ members, likeIds }: Props) {
     ];
 
     function handleTabChange(key: Key) {
-        const params = new URLSearchParams(searchParams);
-        params.set('type', key.toString());
-        router.replace(`${pathname}?${params.toString()}`);
+        startTransition(() => {
+            const params = new URLSearchParams(searchParams);
+            params.set('type', key.toString());
+            router.replace(`${pathname}?${params.toString()}`);
+        });
     }
     return (
         <div className='flex w-full flex-col mt-10 gap-5'>
@@ -38,21 +42,28 @@ export default function ListTab({ members, likeIds }: Props) {
                 {
                     (item) => (
                         <Tab key={item.id} title={item.label}>
-                            {
-                                members.length > 0 ? (
-                                    <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8'>
-                                        {
-                                            members.map(member => (
-                                                <MemberCard key={member.id} member={member} likeIds={likeIds} />
-                                            ))
-                                        }
-                                    </div>
-                                ) : (
-                                    <div>
-                                        No members for this filter.
-                                    </div>
-                                )
-                            }
+                            {isPending ? (
+                                <Loading />
+
+                            ) : (
+                                <>
+                                    {
+                                        members.length > 0 ? (
+                                            <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8'>
+                                                {
+                                                    members.map(member => (
+                                                        <MemberCard key={member.id} member={member} likeIds={likeIds} />
+                                                    ))
+                                                }
+                                            </div>
+                                        ) : (
+                                            <div>
+                                                No members for this filter.
+                                            </div>
+                                        )
+                                    }
+                                </>
+                            )}
                         </Tab>
                     )
                 }
