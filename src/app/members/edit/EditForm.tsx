1be57f68468b413a945b79b9813cbee784df7bae
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-07-24 19:39:30 +0800

    Adding Edit Form components and functionalities.

---
+++ src/app/members/edit/EditForm.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import { MemberEditSchema, memberEditSchema } from '@/lib/schemas/memberEditSchema';
+import { Member } from '@prisma/client'
+import React, { useEffect } from 'react'
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Input, Textarea } from '@heroui/react';
+
+type Props = {
+  member: Member
+}
+
+export default function EditForm({ member }: Props) {
+  const { register, handleSubmit, reset, formState: { isValid, isDirty, isSubmitting, errors } } = useForm<MemberEditSchema>({
+    resolver: zodResolver(memberEditSchema),
+    mode: 'onTouched'
+  });
+
+  useEffect( () => {
+      if (member) {
+        reset({
+          firstName: member.firstName,
+          lastName: member.lastName,
+          description: member.description ?? undefined,
+          city: member.city ?? undefined,
+          country: member.country ?? undefined
+        })
+      }
+    }, [member, reset]);
+
+  const onSubmit = (data: MemberEditSchema) => {
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-4'>
+      <Input 
+        label='First Name'
+        variant='bordered'
+        {...register('firstName')}
+        defaultValue={member.firstName}
+        isInvalid={!!errors.firstName}
+        errorMessage={errors.firstName?.message}
+      />
+      <Input 
+        label='Last Name'
+        variant='bordered'
+        {...register('lastName')}
+        defaultValue={member.lastName}
+        isInvalid={!!errors.lastName}
+        errorMessage={errors.lastName?.message}
+      />
+      <Textarea
+        label='Description'
+        variant='bordered'
+        {...register('description')}
+        defaultValue={member.description ?? undefined}
+        isInvalid={!!errors.description}
+        errorMessage={errors.description?.message}
+        minRows={6}
+      />
+      <div className='flex flex-row gap-3'>
+        <Input 
+          label='City'
+          variant='bordered'
+          {...register('city')}
+          defaultValue={member.city ?? undefined}
+          isInvalid={!!errors.city}
+          errorMessage={errors.city?.message}
+        />
+        <Input 
+          label='Country'
+          variant='bordered'
+          {...register('country')}
+          defaultValue={member.country ?? undefined}
+          isInvalid={!!errors.country}
+          errorMessage={errors.country?.message}
+        />
+      </div>
+      <Button
+        type='submit'
+        className='flex self-end'
+        variant='solid'
+        isDisabled={!isValid || !isDirty}
+        isLoading={isSubmitting}
+        color='secondary'
+      >
+        Update Profile
+      </Button>
+    </form>
+  )
+}
---
+++ src/lib/schemas/memberEditSchema.ts
@@ -0,0 +1,11 @@
+import {z} from 'zod';
+
+export const memberEditSchema = z.object({
+    firstName: z.string().min(1, {message:'Firstname is required'}),
+    lastName: z.string().min(1, {message:'Lastname is required'}),
+    description: z.string().min(1, {message: 'Description is required'}),
+    city: z.string().min(1, {message: 'City is required'}),
+    country: z.string().min(1, {message: 'Country is required'})
+})
+
+export type MemberEditSchema = z.infer<typeof memberEditSchema>
