be28e106a2f3cd74c3aa9dda21cf7dd1bde4b8f4
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-07-28 13:33:55 +0800

    Adding functionality for delete image.

--- src/app/actions/userActions.ts
+++ src/app/actions/userActions.ts
@@ -5,6 +5,7 @@ import { ActionResult } from "@/types";
 import { Member, Photo } from "@prisma/client";
 import { getAuthUserId } from "./authActions";
 import { prisma } from "@/lib/prisma";
+import { cloudinary } from "@/lib/cloudinary";
 
 export async function updateMemberProfile(data: MemberEditSchema, nameUpdated: boolean): Promise<ActionResult<Member>> {
     try {
@@ -18,13 +19,13 @@ export async function updateMemberProfile(data: MemberEditSchema, nameUpdated: b
 
         if (nameUpdated) {
             await prisma.user.update({
-                where: {id: userId},
+                where: { id: userId },
                 data: {
                     name: `${firstName} ${lastName}`
                 }
             });
         }
-        
+
         const member = await prisma.member.update({
             where: { userId },
             data: {
@@ -62,16 +63,36 @@ export async function addImage(url: string, publicId: string) {
     }
 }
 
+export async function deleteImage(photo: Photo) {
+    try {
+        const userId = await getAuthUserId();
+        if (photo.publicId) {
+            await cloudinary.v2.uploader.destroy(photo.publicId);
+        }
+        return prisma.member.update({
+            where: { userId },
+            data: {
+                photos: {
+                    delete: { id: photo.id }
+                }
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
 export async function setMainImage(photo: Photo) {
     try {
         const userId = await getAuthUserId();
         await prisma.user.update({
-            where: {id: userId},
-            data: {image: photo.url}
+            where: { id: userId },
+            data: { image: photo.url }
         });
         return prisma.member.update({
-            where: {userId},
-            data: {image: photo.url}
+            where: { userId },
+            data: { image: photo.url }
         })
     } catch (error) {
         console.log(error);
@@ -83,8 +104,8 @@ export async function getUserInfoForNav() {
     try {
         const userId = await getAuthUserId();
         return prisma.user.findUnique({
-            where: {id: userId},
-            select: {name: true, image: true}
+            where: { id: userId },
+            select: { name: true, image: true }
         })
     } catch (error) {
         console.log(error);
--- src/components/MemberPhotos.tsx
+++ src/components/MemberPhotos.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { setMainImage } from '@/app/actions/userActions';
+import { deleteImage, setMainImage } from '@/app/actions/userActions';
 import { Photo } from '@prisma/client';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
@@ -28,6 +28,13 @@ export default function MemberPhotos({ photos, editing, mainImageUrl }: Props) {
         router.refresh();
         setLoading({ isLoading: false, type: '', id: '' });
     }
+    const onDelete = async (photo: Photo) => {
+        if (photo.url === mainImageUrl) return null;
+        setLoading({ isLoading: true, type: 'delete', id: photo.id });
+        await deleteImage(photo);
+        router.refresh();
+        setLoading({ isLoading: false, type: '', id: '' });
+    }
 
     return (
         <div className='grid grid-cols-5 gap-3 p-5'>
@@ -39,8 +46,8 @@ export default function MemberPhotos({ photos, editing, mainImageUrl }: Props) {
                             <div onClick={() => onSetMain(photo)} className='absolute top-3 left-3 z-50'>
                                 <StarButton selected={photo.url === mainImageUrl} loading={loading.isLoading && loading.type === 'main' && loading.id === photo.id} />
                             </div>
-                            <div className='absolute top-3 right-3 z-50'>
-                                <DeleteButton loading={false} />
+                            <div onClick={() => onDelete(photo)} className='absolute top-3 right-3 z-50'>
+                                <DeleteButton loading={loading.isLoading && loading.type === 'delete' && loading.id === photo.id} />
                             </div>
                         </>
                     )}
