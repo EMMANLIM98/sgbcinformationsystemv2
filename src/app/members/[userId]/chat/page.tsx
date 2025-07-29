a918061f1ad17c1a2c5889d11f67d2c61bc0ad37
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-07-29 14:39:16 +0800

    Displaying messages part 1.

--- src/app/members/[userId]/chat/page.tsx
+++ src/app/members/[userId]/chat/page.tsx
@@ -2,17 +2,30 @@ import CardInnerWrapper from '@/components/CardInnerWrapper'
 import React from 'react'
 import ChatForm from './ChatForm'
 import { getMessageThread } from '@/app/actions/messageActions'
+import MessageBox from './MessageBox';
 
 export default async function ChatPage({ params }: { params: Promise<{ userId: string }> }) {
   const { userId } = await params;
   const messages = await getMessageThread(userId);
 
-  console.log(messages);
+  const body = (
+    <div>
+      {messages.length === 0 ? 'No messages yet' : (
+        <div>
+          {
+            messages.map(message => (
+              <MessageBox key={message.id} message={message} currentUserId={userId} />
+            ))
+          }
+        </div>
+      )}
+    </div>
+  )
 
   return (
     <CardInnerWrapper
       header='Chat'
-      body={<div>Chat messages will appear here</div>}
+      body={body}
       footer={<ChatForm />}
     />
   )
