0483d43fe6361a45aff35d2ce843e35c2bf1c9fb
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-07-31 21:16:16 +0800

    Using presence channel hook.

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,8 +1,10 @@
 'use client';
+import { usePresenceChannel } from "@/hooks/usePresenceChannel";
 import { HeroUIProvider } from "@heroui/system";
 import { ToastContainer } from "react-toastify";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  usePresenceChannel();
   return (
     <HeroUIProvider>
       <ToastContainer position="bottom-right" className="z-50" />
