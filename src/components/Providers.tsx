212017bb5aa2fab7ccc71fccf2350a89cef7c762
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-08-01 14:56:54 +0800

    Setting up private channel.

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,10 +1,12 @@
 'use client';
+import { useNotificationChannel } from "@/hooks/useNotificationChannel";
 import { usePresenceChannel } from "@/hooks/usePresenceChannel";
 import { HeroUIProvider } from "@heroui/system";
 import { ToastContainer } from "react-toastify";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({ children, userId }: { children: React.ReactNode, userId: string | null}) {
   usePresenceChannel();
+  useNotificationChannel(userId);
   return (
     <HeroUIProvider>
       <ToastContainer position="bottom-right" className="z-50" />
