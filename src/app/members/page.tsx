44a79372bbab9b098b913d0835e4eaa2e1b89459
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-08-05 22:14:20 +0800

    Adding UI for pagination.

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { getMembers } from '../actions/memberActions'
 import MemberCard from './MemberCard'
 import { fetchCurrentUserLikeIds } from '../actions/likeActions';
+import { Pagination } from '@heroui/react';
+import PaginationComponent from '@/components/PaginationComponent';
 
 
 export default async function MembersPage() {
@@ -9,10 +11,13 @@ export default async function MembersPage() {
   const likeIds = await fetchCurrentUserLikeIds();
 
   return (
-    <div className='mt-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8'>
-      {members && members.map(member => (
-        <MemberCard member={member} key={member.id} likeIds={likeIds}/>
-      ))}
-    </div>
+    <>
+      <div className='mt-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8'>
+        {members && members.map(member => (
+          <MemberCard member={member} key={member.id} likeIds={likeIds} />
+        ))}
+      </div>
+      <PaginationComponent />
+    </>
   )
 }
