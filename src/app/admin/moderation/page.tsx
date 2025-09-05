1aa7c2930656856619c88dbab3c7a26b7cf178f2
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-09-05 21:29:25 +0400

    Preparing the app for publishing.

--- src/app/admin/moderation/page.tsx
+++ src/app/admin/moderation/page.tsx
@@ -3,6 +3,8 @@ import MemberPhotos from '@/components/MemberPhotos';
 import { Divider } from '@heroui/react';
 import React from 'react'
 
+export const dynamic = 'force-dynamic';
+
 export default async function PhotoModerationPage() {
   const photos = await getUnapprovedPhotos();
   return (
