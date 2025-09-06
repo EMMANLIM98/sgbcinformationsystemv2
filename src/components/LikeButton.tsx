7b16904fac3b5e71e4bb160bfe5bb2a55b1c5d03
Emman Jr. Lim <emman_lim_98@yahoo.com>
2025-09-06 13:22:40 +0400

    Fixing small bug in LikeButton.

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -13,12 +13,12 @@ type Props = {
 export default function LikeButton({ loading, toggleLike, hasLiked }: Props) {
     return (
         <>
-            {!loading && (
+            {!loading ? (
                 <div onClick={toggleLike} className='relative hover:opacity-80 transition cursor-pointer'>
                     <AiOutlineHeart size={28} className='fill-white absolute -top-[2px] -right-[2px]' />
                     <AiFillHeart size={24} className={hasLiked ? 'fill-rose-500' : 'fill-gray-500/70'} />
                 </div>
-            ): (
+            ) : (
                 <PiSpinnerGap size={32} className='fill-white animate-spin'/>
             )}
         </>
