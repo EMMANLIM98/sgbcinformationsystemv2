1c413956b19a92257a009e80318173b37463d9ae
Emmanuel Jr Lim <EmmanuelJrLim@alsafaroadcontracting.com>
2025-11-07 14:34:09 +0400

    Fixing filters media queries.

--- src/components/navbar/Filters.tsx
+++ src/components/navbar/Filters.tsx
@@ -8,30 +8,33 @@ export default function Filters() {
     const { orderbyList, genderList, selectAge, selectGender, selectOrder, filters, isPending, totalCount, selectWithPhoto } = useFilters();
 
     return (
-        <div className='shadow-md py-2'>
-            <div className='flex flex-row justify-around items-center'>
-                <div className='flex gap-2 items-center'>
-                    <div className='text-secondary font-semibold text-xl'>
+        <div className='shadow-md py-2 px-2 md:px-0 filters-mobile'>
+            <div className='flex flex-col md:flex-row md:justify-around md:items-center gap-3'>
+                <div className='flex gap-2 items-center justify-start md:justify-center w-full md:w-auto'>
+                    <div className='text-secondary font-semibold text-sm md:text-xl result-count'>
                         Results: {isPending ? <Spinner size='sm' color='secondary' /> : totalCount}
                     </div>
-
                 </div>
 
-                <div className='flex gap-2 items-center'>
-                    <div>Gender:</div>
-                    {genderList.map(({ icon: Icon, label }) => (
-                        <Button
-                            key={label}
-                            size='sm'
-                            isIconOnly
-                            color={filters.gender.includes(label) ? 'secondary' : 'default'}
-                            onPress={() => selectGender(label)}
-                        >
-                            <Icon size={24} />
-                        </Button>
-                    ))}
+                <div className='flex gap-2 items-center w-full md:w-auto'>
+                    <div className='text-sm md:text-base mr-1'>Gender:</div>
+                    <div className='flex gap-2 items-center'>
+                        {genderList.map(({ icon: Icon, label }) => (
+                            <Button
+                                key={label}
+                                size='sm'
+                                isIconOnly
+                                color={filters.gender.includes(label) ? 'secondary' : 'default'}
+                                onPress={() => selectGender(label)}
+                            >
+                                <Icon size={24} />
+                            </Button>
+                        ))}
+                    </div>
                 </div>
-                <div className='flex flex-row items-center gap-2 w-1/4'>
+
+                <div className='flex items-center gap-2 w-full md:w-1/4 slider-wrapper'>
+                    <div className='hidden md:block text-sm'>Age Range</div>
                     <Slider
                         aria-label='Slider for age selection'
                         label='Age Range'
@@ -43,8 +46,9 @@ export default function Filters() {
                         onChangeEnd={(value) => selectAge(value as number[])}
                     />
                 </div>
-                <div className='flex flex-col items-center'>
-                    <p className='text-sm'>With Photo</p>
+
+                <div className='flex flex-col items-center w-full md:w-auto'>
+                    <p className='text-sm md:text-base'>With Photo</p>
                     <Switch 
                         color='secondary'
                         defaultSelected
@@ -52,7 +56,8 @@ export default function Filters() {
                         onChange={selectWithPhoto}
                     />
                 </div>
-                <div className='w-1/4'>
+
+                <div className='w-full md:w-1/4'>
                     <Select
                         size='sm'
                         fullWidth
