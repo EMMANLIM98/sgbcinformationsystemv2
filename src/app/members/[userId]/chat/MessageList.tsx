9a2e44d8cf756daa548d4974075bfd4bc6c782de
Emmanuel Jr Lim <emman_lim_98@yahoo.com>
2025-11-22 07:32:45 +0400

    Fixing mispelled word formatShrotDateTime and dependencies..

--- src/app/members/[userId]/chat/MessageList.tsx
+++ src/app/members/[userId]/chat/MessageList.tsx
@@ -1,79 +1,84 @@
-'use client';
+"use client";
 
-import React, { useCallback, useEffect, useRef, useState } from 'react'
-import MessageBox from './MessageBox';
-import { MessageDto } from '@/types';
-import { pusherClient } from '@/lib/pusher';
-import { formatShrotDateTime } from '@/lib/util';
-import { Channel } from 'pusher-js';
-import useMessageStore from '@/hooks/useMessageStore';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import MessageBox from "./MessageBox";
+import { MessageDto } from "@/types";
+import { pusherClient } from "@/lib/pusher";
+import { formatShortDateTime } from "@/lib/util";
+import { Channel } from "pusher-js";
+import useMessageStore from "@/hooks/useMessageStore";
 
 type Props = {
-    initialMessages: { messages: MessageDto[], readCount: number }
-    currentUserId: string
-    chatId: string
-}
+  initialMessages: { messages: MessageDto[]; readCount: number };
+  currentUserId: string;
+  chatId: string;
+};
 
-export default function MessageList({ initialMessages, currentUserId, chatId }: Props) {
-    const channelRef = useRef<Channel | null>(null);
-    const [messages, setMessages] = useState(initialMessages.messages);
-    const updatedReadCount = useMessageStore(state => state.updateUnreadCount);
-    const setReadCount = useRef(false);
+export default function MessageList({
+  initialMessages,
+  currentUserId,
+  chatId,
+}: Props) {
+  const channelRef = useRef<Channel | null>(null);
+  const [messages, setMessages] = useState(initialMessages.messages);
+  const updatedReadCount = useMessageStore((state) => state.updateUnreadCount);
+  const setReadCount = useRef(false);
 
-    useEffect(() => {
-        if (!setReadCount.current) {
-            updatedReadCount(-initialMessages.readCount);
-            setReadCount.current = true;
-        }
+  useEffect(() => {
+    if (!setReadCount.current) {
+      updatedReadCount(-initialMessages.readCount);
+      setReadCount.current = true;
+    }
+  }, [initialMessages.readCount, updatedReadCount]);
 
-    }, [initialMessages.readCount, updatedReadCount]);
+  const handleNewMessage = useCallback((message: MessageDto) => {
+    setMessages((prevState) => {
+      return [...prevState, message];
+    });
+  }, []);
 
-    const handleNewMessage = useCallback((message: MessageDto) => {
-        setMessages(prevState => {
-            return [...prevState, message]
-        })
-    }, []);
+  const handleReadMessages = useCallback((messageIds: string[]) => {
+    setMessages((prevState) =>
+      prevState.map((message) =>
+        messageIds.includes(message.id)
+          ? { ...message, dateRead: formatShortDateTime(new Date()) }
+          : message
+      )
+    );
+  }, []);
 
-    const handleReadMessages = useCallback((messageIds: string[]) => {
-        setMessages(prevState => prevState.map(message => messageIds.includes(message.id)
-            ? { ...message, dateRead: formatShrotDateTime(new Date()) }
-            : message
-        ));
-    }, []);
+  useEffect(() => {
+    if (!channelRef.current) {
+      channelRef.current = pusherClient.subscribe(chatId);
 
-    useEffect(() => {
-        if (!channelRef.current) {
-            channelRef.current = pusherClient.subscribe(chatId);
+      channelRef.current.bind("message:new", handleNewMessage);
+      channelRef.current.bind("messages:read", handleReadMessages);
+    }
 
-            channelRef.current.bind('message:new', handleNewMessage);
-            channelRef.current.bind('messages:read', handleReadMessages);
-        }
+    return () => {
+      if (channelRef.current && channelRef.current.subscribed) {
+        channelRef.current.unsubscribe();
+        channelRef.current.unbind("message:new", handleNewMessage);
+        channelRef.current.unbind("messages:read", handleReadMessages);
+      }
+    };
+  }, [chatId, handleNewMessage, handleReadMessages]);
 
-        return () => {
-            if (channelRef.current && channelRef.current.subscribed) {
-                channelRef.current.unsubscribe();
-                channelRef.current.unbind('message:new', handleNewMessage);
-                channelRef.current.unbind('messages:read', handleReadMessages);
-            }
-
-        }
-    }, [chatId, handleNewMessage, handleReadMessages]);
-
-    return (
+  return (
+    <div>
+      {messages.length === 0 ? (
+        "No messages yet"
+      ) : (
         <div>
-            {messages.length === 0 ? 'No messages yet' : (
-                <div>
-                    {
-                        messages.map(message => (
-                            <MessageBox
-                                key={message.id}
-                                message={message}
-                                currentUserId={currentUserId}
-                            />
-                        ))
-                    }
-                </div>
-            )}
+          {messages.map((message) => (
+            <MessageBox
+              key={message.id}
+              message={message}
+              currentUserId={currentUserId}
+            />
+          ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
--- src/lib/mappings.ts
+++ src/lib/mappings.ts
@@ -1,19 +1,19 @@
 import { MessageWithSenderRecipient } from "@/types";
-import { formatShrotDateTime } from "./util";
+import { formatShortDateTime } from "./util";
 
 export function mapMessageToMessageDto(message: MessageWithSenderRecipient) {
-    return {
-        id: message.id,
-        text: message.text,
-        created: formatShrotDateTime(message.created),
-        dateRead: message.dateRead ? formatShrotDateTime(message.dateRead) : null,
-        senderId: message.sender?.userId,
-        senderFirstName: message.sender?.firstName,
-        senderLastName: message.sender?.lastName,
-        senderImage: message.sender?.image,
-        recipientId: message.recipient?.userId,
-        recipientFirstName: message.recipient?.firstName,
-        recipientLastName: message.recipient?.lastName,
-        recipientImage: message.recipient?.image
-    }
+  return {
+    id: message.id,
+    text: message.text,
+    created: formatShortDateTime(message.created),
+    dateRead: message.dateRead ? formatShortDateTime(message.dateRead) : null,
+    senderId: message.sender?.userId,
+    senderFirstName: message.sender?.firstName,
+    senderLastName: message.sender?.lastName,
+    senderImage: message.sender?.image,
+    recipientId: message.recipient?.userId,
+    recipientFirstName: message.recipient?.firstName,
+    recipientLastName: message.recipient?.lastName,
+    recipientImage: message.recipient?.image,
+  };
 }
