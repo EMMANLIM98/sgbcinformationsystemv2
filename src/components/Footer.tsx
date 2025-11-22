a896c29c57cf5f536850795798f31e97cd3a5cdd
Emmanuel Jr Lim <emman_lim_98@yahoo.com>
2025-11-22 12:32:00 +0400

    Changes in footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,8 @@ export default function Footer() {
     <footer className="w-full site-footer text-center py-4 site-footer--fixed-mobile">
       <div className="container mx-auto flex items-center justify-between px-4 md:px-0">
         <div className="text-sm hidden md:block">
-          © {new Date().getFullYear()} SGBC Information System by ITM
+          © {new Date().getFullYear()} SGBC Information System by SGBC (Main)
+          ITM
         </div>
         <div className="flex gap-4 items-center">
           <Link href="/privacy" className="text-sm hover:underline">
